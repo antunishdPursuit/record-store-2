7263e2d688b902a7796141b369420db4ee33a1a0
j-goodman <johngoodman@pursuit.org>
2023-02-21 18:21:45 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,6 @@
+let displayedRecords = allRecords
+
+// Do not change the code below this line.
+window.addEventListener("load", () => {
+    recordStoreHtmlMethods.fillRecordContainer(displayedRecords);
+});
